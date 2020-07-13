4240d0451e8231d41dea71d232bdbb5e29d4d9c8
h-saitocm <1770hina@gmail.com>
2020-07-13 19:25:03 +0900

    【第１０部 - 投稿データを保存しよう】

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-});
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');

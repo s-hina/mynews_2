d9fd2f25d7a010b46b6b75b8fd628b108ea21048
h-saitocm <1770hina@gmail.com>
2020-07-16 17:31:52 +0900

    【第１４部 - ニュースサイトを作成しよう】

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
